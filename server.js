b3948fa3ba5a852ca4b8e6d7f857aca15e679358
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2023-01-25 18:15:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let ytdl = require('ytdl-core');
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3005, () => {
+    console.log('Ytdl api is up.')
+})
+
+app.post('/api/yt/download', (req, res) => {
+    try {
+        console.log(req.body)
+        if (ytdl.validateURL(req.body.url)) {
+            if (req.body.type == 'mp3') {
+                res.header('Content-Disposition', 'attachment; filename="' + new Date() + '- Fizzy.mp3"');
+                ytdl(req.body.url, { format: 'mp3', filter: 'audioandvideo', quality: 'highest' }).pipe(res);
+            } else {
+                if (req.body.type == 'mp4') {
+                    res.header('Content-Disposition', 'attachment; filename="' + new Date() + '- Fizzy.mp4"');
+                    ytdl(req.body.url, { format: 'mp4', filter: 'audioandvideo', quality: 'highest' }).pipe(res);
+                }
+            }
+        } else {
+            res.json({ error: 'url not found', status: 404 })
+        }
+    } catch (err) {
+        res.json({ error: err, status: 500 });
+    }
+})
